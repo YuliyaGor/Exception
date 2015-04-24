4614105dd8786db702fd11deb574960395a22c95
YuliyaGor <d_w@ukr.net>
2015-04-24 10:09:46 +0300

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,28 @@
+package exception;
+
+public class Arithmetic {
+	
+	private int a;
+	private int b;
+	
+	public Arithmetic() {
+		
+	}
+
+	public Arithmetic(int x, int y) {
+		this.a = x;
+		this.b = y;
+	}
+	
+	public double div() throws ArithmeticException {
+		double result;
+		try {
+			result = this.a/this.b;
+		} catch (ArithmeticException e) {
+			result = 0;
+			throw e;
+		}
+		
+		return result;
+	}
+}
